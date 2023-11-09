00435e0728c57e566dce422745bf9722d38fc508
Yandri30 <144971806+Yandri30@users.noreply.github.com>
2023-11-09 00:57:51 -0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App'; // Actualiza la ruta según la ubicación real de tu componente App
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
